dc6cb82bc770d2747b464332dac1cab12ad8b12f
unknown <tasler.luka@gmail.com>
2018-02-22 23:00:50 +0100

    A working prototype

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class Paddle : MonoBehaviour {
+
+	[SerializeField]
+	[Range(1, 2)]
+	int playerNumber;
+	[SerializeField]
+	float speed;
+
+	CharacterController charController;
+
+
+	void Awake() {
+		charController = GetComponent<CharacterController>();
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float x = Input.GetAxis("Horizontal_P" + playerNumber) * speed * Time.deltaTime;
+		float y = Input.GetAxis("Vertical_P" + playerNumber) * speed * Time.deltaTime;
+		Vector3 motion = new Vector3(x, y, 0);
+
+		charController.Move(motion);
+	}
+
+	public Vector3 GetBounceDirection(Collision collision) {
+		Vector3 collisionLocation = Vector3.zero;
+		foreach (ContactPoint c in collision.contacts)
+			collisionLocation += c.point;
+		collisionLocation /= collision.contacts.Length;
+		collisionLocation = transform.InverseTransformPoint(collisionLocation);
+
+		Vector3 newDirection = new Vector3(collisionLocation.x, .5f, collisionLocation.z).normalized;
+
+		Debug.DrawRay(transform.TransformPoint(collisionLocation), transform.TransformDirection(transform.forward), Color.red, 10f);
+		Debug.DrawRay(transform.TransformPoint(collisionLocation), transform.TransformDirection(newDirection), Color.blue, 10f);
+
+		return transform.TransformDirection(newDirection);
+	}
+
+	void OnDrawGizmos() {
+		Gizmos.color = Color.yellow;
+		Gizmos.DrawRay(transform.position, transform.TransformDirection(transform.forward));
+	}
+}
---
+++ Assets/Scripts/Puck.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Puck : MonoBehaviour {
+
+	[SerializeField]
+	float initialSpeed = 10f;
+	[SerializeField]
+	float speedupOnHit = .5f;
+
+	Rigidbody rigidBody;
+
+	//it's required for bouncing
+	Vector3 velocity;
+
+	void Awake() {
+		rigidBody = GetComponent<Rigidbody>();
+	}
+
+	// Use this for initialization
+	void Start () {
+		InitBall();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void InitBall() {
+		Vector3 direction = Vector3.zero;
+		direction.x = Random.Range(-.8f, .8f);
+		direction.y = Random.Range(-.8f, .8f);
+		direction.z = (int)Random.Range(0, 2) * 2 - 1;
+		Debug.Log("Ball direction: " + direction);
+		direction.Normalize();
+
+		rigidBody.velocity = velocity = direction * initialSpeed;
+
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		Paddle paddle = collision.gameObject.GetComponent<Paddle>();
+		Vector3 newDirection;
+		if (paddle)
+			newDirection = paddle.GetBounceDirection(collision);
+		else {
+			Vector3 collisonNormal = Vector3.zero;
+			foreach (ContactPoint c in collision.contacts)
+				collisonNormal += c.normal;
+			collisonNormal /= collision.contacts.Length;
+
+			newDirection = Vector3.Reflect(velocity.normalized, collisonNormal);
+		}
+
+		float speed = velocity.magnitude;
+		speed += speedupOnHit;
+		rigidBody.velocity = velocity = newDirection * speed;
+	}
+
+	void OnTriggerEnter(Collider other) {
+		Debug.Log(other.gameObject);
+		//TODO: keep track of score
+		//reset d pozition.
+		transform.position = new Vector3(0, 0, 0);
+		InitBall();
+	}
+}
