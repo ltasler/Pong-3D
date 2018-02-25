abae49abeb25e88476d4d7a6809bbd82f3ba5c32
unknown <tasler.luka@gmail.com>
2018-02-25 21:13:59 +0100

    Score tracking

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : Singleton<GameManager> {
+
+	[SerializeField]
+	Text _textP1;
+	[SerializeField]
+	Text _textP2;
+
+	string _scoreP1 = "0";
+	string _scoreP2 = "0";
+
+	void OnEnable() {
+		Puck.OnPlayerScored += PlayerScored;
+	}
+
+	void OnDisable() {
+		Puck.OnPlayerScored += PlayerScored;
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnGUI() {
+		if (_textP1 != null)
+			_textP1.text = _scoreP1.ToString();
+		else
+			Debug.Log("[Game Manager - OnGUI] No text prefab for player 1");
+		if (_textP2 != null)
+			_textP2.text = _scoreP2.ToString();
+		else
+			Debug.Log("[Game Manager - OnGUI] No text prefab for player 2");
+	}
+
+	private void PlayerScored(int player) {
+		int socreP2 = int.Parse(_scoreP2);
+		if (player == 1)
+			_scoreP1 = (int.Parse(_scoreP1) + 1).ToString();
+		else if (player == 2)
+			_scoreP2 = (int.Parse(_scoreP2) + 1).ToString();
+		else
+			Debug.LogWarning("[Game Manager - PlayerScored] Uknown player number: " + player);
+
+	}
+
+}
--- Assets/Scripts/Puck.cs
+++ Assets/Scripts/Puck.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class Puck : MonoBehaviour {
 
+	public delegate void PlayerScored(int player);
+	public static event PlayerScored OnPlayerScored;
+
 	[SerializeField]
 	float initialSpeed = 10f;
 	[SerializeField]
@@ -21,7 +24,7 @@ public class Puck : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		InitBall();
+		StartCoroutine(Respawn());
 	}
 	
 	// Update is called once per frame
@@ -29,24 +32,14 @@ public class Puck : MonoBehaviour {
 		
 	}
 
-	void InitBall() {
-		Vector3 direction = Vector3.zero;
-		direction.x = Random.Range(-.8f, .8f);
-		direction.y = Random.Range(-.8f, .8f);
-		direction.z = (int)Random.Range(0, 2) * 2 - 1;
-		Debug.Log("Ball direction: " + direction);
-		direction.Normalize();
-
-		rigidBody.velocity = Velocity = direction * initialSpeed;
-
-	}
-
 	void OnCollisionEnter(Collision collision) {
 		Paddle paddle = collision.gameObject.GetComponent<Paddle>();
 		Vector3 newDirection;
-		if (paddle)
+		float speed = Velocity.magnitude;
+		if (paddle) {
 			newDirection = paddle.GetBounceDirection(collision);
-		else {
+			speed += speedupOnHit;
+		} else {
 			Vector3 collisonNormal = Vector3.zero;
 			foreach (ContactPoint c in collision.contacts)
 				collisonNormal += c.normal;
@@ -54,17 +47,43 @@ public class Puck : MonoBehaviour {
 
 			newDirection = Vector3.Reflect(Velocity.normalized, collisonNormal);
 		}
-
-		float speed = Velocity.magnitude;
-		speed += speedupOnHit;
 		rigidBody.velocity = Velocity = newDirection * speed;
 	}
 
 	void OnTriggerEnter(Collider other) {
-		Debug.Log(other.gameObject);
-		//TODO: keep track of score
-		//reset d pozition.
+		BroadcastPlayerScored(other);
+		StartCoroutine(Respawn());
+	}
+
+	private IEnumerator Respawn() {
 		transform.position = new Vector3(0, 0, 0);
+		rigidBody.velocity = Velocity = Vector3.zero;
+		yield return new WaitForSeconds(1);
 		InitBall();
+
+	}
+
+	private void BroadcastPlayerScored(Collider other) {
+		if (OnPlayerScored == null)
+			return;
+		int player = 0;
+		string otherName = other.gameObject.name;
+		if (otherName.Contains("P1"))
+			player = 1;
+		else if (otherName.Contains("P2"))
+			player = 2;
+		OnPlayerScored(player);
+	}
+
+	private void InitBall() {
+		Vector3 direction = Vector3.zero;
+		direction.x = Random.Range(-.8f, .8f);
+		direction.y = Random.Range(-.8f, .8f);
+		direction.z = (int)Random.Range(0, 2) * 2 - 1;
+		direction.Normalize();
+		Debug.Log("Ball direction: " + direction);
+
+		rigidBody.velocity = Velocity = direction * initialSpeed;
+
 	}
 }
