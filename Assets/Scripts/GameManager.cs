7bc415c34d35ba9cca26f1f499d81a4e4d788e04
unknown <tasler.luka@gmail.com>
2018-03-02 23:20:56 +0100

    Added Menu, modifyed Game Manager for it.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,29 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class GameManager : Singleton<GameManager> {
-
-	[SerializeField]
+public class GameManager : MonoBehaviour {
+	
 	Text _textP1;
-	[SerializeField]
 	Text _textP2;
 
 	string _scoreP1 = "0";
 	string _scoreP2 = "0";
+
+	const string SCENE_MAIN = "Main";
 
 	void OnEnable() {
 		Puck.OnPlayerScored += PlayerScored;
 	}
 
 	void OnDisable() {
-		Puck.OnPlayerScored += PlayerScored;
+		Puck.OnPlayerScored -= PlayerScored;
+	}
+
+	void Awake() {
+		DontDestroyOnLoad(this);
 	}
 
 	// Use this for initialization
 	void Start () {
 
+	}
+
+	void OnLevelWasLoaded() {
+		LoadScoreUi();
 	}
 	
 	// Update is called once per frame
@@ -31,25 +40,58 @@ public class GameManager : Singleton<GameManager> {
 		
 	}
 
+	/// <summary>
+	/// OnGUI is only supposed to be used when in game
+	/// </summary>
 	void OnGUI() {
 		if (_textP1 != null)
 			_textP1.text = _scoreP1.ToString();
-		else
-			Debug.Log("[Game Manager - OnGUI] No text prefab for player 1");
 		if (_textP2 != null)
 			_textP2.text = _scoreP2.ToString();
-		else
-			Debug.Log("[Game Manager - OnGUI] No text prefab for player 2");
 	}
 
+	#region Menu Scene button clicks
+
+	public void OnePlayer() {
+		//Todo: Implement it!
+	}
+
+	public void TwoPlayers() {
+		SceneManager.LoadScene(SCENE_MAIN);
+	}
+
+	public void Exit() {
+#if UNITY_EDITOR 
+		UnityEditor.EditorApplication.isPlaying = false;
+#else
+		Application.Quit();
+#endif
+	}
+
+#endregion
+
 	private void PlayerScored(int player) {
-		int socreP2 = int.Parse(_scoreP2);
 		if (player == 1)
 			_scoreP1 = (int.Parse(_scoreP1) + 1).ToString();
 		else if (player == 2)
 			_scoreP2 = (int.Parse(_scoreP2) + 1).ToString();
 		else
 			Debug.LogWarning("[Game Manager - PlayerScored] Uknown player number: " + player);
+
+	}
+
+	private void LoadScoreUi() {
+		GameObject scoreUi = GameObject.FindGameObjectWithTag("ScoreUI");
+		if (scoreUi == null)
+			return;
+
+		Text[] texts = scoreUi.GetComponentsInChildren<Text>();
+		foreach(Text t in texts) {
+			if (t.gameObject.name.Contains("Red"))
+				_textP1 = t;
+			else
+				_textP2 = t;
+		}
 
 	}
 
